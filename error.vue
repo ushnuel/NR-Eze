173c96bff2ec8668d9bf3c3e6c16fe20313ec7ab
Emmanuel Eze <44417422+ushnuel@users.noreply.github.com>
2024-10-07 12:16:23 +0100

    feat: adds vue error page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+import { Button } from "./components/ui/button";
+
+defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center gap-4">
+    <h1 class="text-2xl text-red-500">{{ error?.statusCode }} Error</h1>
+    <h1>{{ error?.statusMessage }}</h1>
+    <Button @click="handleError" :size="'lg'" :variant="'secondary'"
+      >Refresh</Button
+    >
+  </div>
+</template>
