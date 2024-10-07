6226908638254b883d5d9829e8d72ef57d245cc9
Emmanuel Eze <44417422+ushnuel@users.noreply.github.com>
2024-10-07 12:15:57 +0100

    feat: updates the app vue, include the pages, toaster and loading indicator

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <Toaster />
+  <main>
+    <AppHeader />
+    <NuxtLoadingIndicator />
+    <div class="mt-20 p-5 sm:px-10 min-h-screen">
+      <AppNav class="flex sm:hidden mb-5" />
+      <NuxtPage />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Toaster from "@/components/ui/toast/Toaster.vue";
+</script>
