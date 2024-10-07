05d490ea011300c514d52b20d309b7a0086a90dc
Emmanuel Eze <44417422+ushnuel@users.noreply.github.com>
2024-10-07 12:15:12 +0100

    feat: add launch and save-launch pages

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <p v-if="status === 'pending'">Please wait...</p>
+  <div v-else>
+    <TableDetails :launches="launchStore.launches" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TableDetails from "~/components/TableDetails.vue";
+
+const launchStore = useLaunchStore();
+
+const { status } = await useAsyncData("launches", () =>
+  launchStore.getLaunches()
+);
+</script>
---
+++ pages/saved-launches/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid auto-fit-[250px] gap-3">
+    <div v-for="launch in launchStore.saved_launches" :key="launch.id">
+      <LaunchCard :launch="launch" />
+    </div>
+  </div>
+  <div v-if="launchStore.saved_launches.length === 0">
+    <p>No Saved Launches</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const launchStore = useLaunchStore();
+
+await useAsyncData("launches", () => launchStore.getSaved());
+</script>
